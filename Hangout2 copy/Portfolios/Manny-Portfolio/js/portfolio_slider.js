d1fa000df2b3ad584e640c114d7cdf77799e3379
Manuel Flores <mfloramirez11@gmail.com>
2015-09-04 10:58:20 -0700

    Added .gitignore file

---
+++ Hangout2 copy/Portfolios/Manny-Portfolio/js/portfolio_slider.js
@@ -0,0 +1,67 @@
+$(function(){
+$("#elastic_grid_demo").elastic_grid({	
+	'hoverDirection': true,
+	'hoverDelay': 0,
+	'hoverInverse': false,
+	'expandingSpeed': 500,
+	'expandingHeight': 500,
+	'items' :
+		[
+			{
+			'title' : 'Hangout',
+			'description'   : 'This is an interactive website that will allow people within the same city to quickly set-up meet up spots for Academic, Sport, Nightlife, Extra-Curricular Activities, or any Miscellaneous Events. At a drop of a pin. It turns boredom into fun. Designed to help anyone find a place to meet, play, and study with. Your playground at the tip of your fingers. Do not let your free time go to waste',
+			'thumbnail' : ['img/hands.jpg', 'img/MAPSMALL.PNG', 
+'img/LAWLS.PNG', 
+'img/Screenshot1.png'],
+			'large' : ['img/AAaDDDSFS.png', 'img/MAPLARGE.PNG', 'img/lalalala.png', 'img/Screenshot .png'],
+			'button_list'   :
+			[
+			
+			],
+			'tags'  : ['Mobile App','CSS/HTML','All' ]
+			},
+
+			{
+			'title' : 'Kickback Learning by Technotron',
+			'description'   : 'The tutoring Uber. This App was designed to allow students to tutor other students at a low cost and at the ease of their finger tips',
+			'thumbnail' : ['img/Screenshot4.png', 'img/Screenshot5.png', 'img/Screenshot8.png'],
+			'large' : ['img/Screenshot3.png', 'img/Screenshot6.png', 'img/Screenshot7.png'],
+			'button_list'   :
+			[
+			
+			],
+			'tags'  : ['Mobile App','All']
+			},
+
+			{
+			'title' : 'Water Sensor by Town and Country',
+			'description'   : 'A Garden watering mechanism that was controlled by a raspberry pi and then evolved into a UX design to educate students in the form of agriculture and instill STEM career paths. ',
+			'thumbnail' : ['img/Screenshot88.png',
+                           'img/Screenshot10.png',
+                           'img/Screenshot12.png'],
+			'large' : ['img/Screenshot9.png','img/Screenshot111.png', 'img/Screenshot13.png'],
+			'button_list'   :
+			[
+			
+			],
+			'tags'  : ['Design', 'All']
+			},
+
+			{
+			'title' : 'Bucketbots by Carpa CAMS',
+			'description'   : 'The goal was to build a modular robot that self assembles itself, and can be rearranged to anything within the parameters to fufill a course or set of instruction. All and all this project was on a 1000 dollar budget',
+			'thumbnail' : ['img/A1.png', 'img/A2.png', 'img/A4.png'],
+			'large' : ['img/A6.png', 'img/A7.png', 'img/A8.png'],
+			'button_list'   :
+			[
+			
+			],
+			'tags'  : ['Design','Arduino','All']
+			},
+
+			
+
+			
+		]
+	});
+});
